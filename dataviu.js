ddda78e1a48173a582fbdb9547ce9d0fa205a44a
ptrk <ptrk@ntxt.net>
2018-07-07 22:36:25 +0200

    initial commit - resizing handled for a div and a canvas differently

---
+++ dataviu.js
@@ -0,0 +1,62 @@
+$(document).ready(init);
+
+function init(){
+    var logLine = 0;
+    var resizeCallbacks = [
+        function resizeDiv(){
+            var me = $("div.fluidsize");
+            me.html("szer: " + me.css("width"));
+        },
+        function resizeCanvas(){
+            var me = $("canvas.fluidsize");
+            var canvas = me[0];
+
+            if(adjustCanvas(canvas)){
+                var ctx = canvas.getContext("2d");
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.font = "30px Arial";
+                ctx.fillText(canvas.clientWidth,10,30);
+            }
+        }
+    ];
+
+
+    $( window ).resize(throttle(runCallbacks, 200));
+
+    function runCallbacks() {
+        resizeCallbacks.forEach(function(cb){
+            cb.call();
+        });
+    }
+
+    function throttle(callback, delay) {
+        var timer;
+        return function throttlingWrapper(e){
+            var that = this;
+            if(timer) clearTimeout(timer);
+            timer = setTimeout(
+                function(){ callback.call(that, e); },
+                delay
+            );
+        }
+    }
+
+    function adjustCanvas(canvas){
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        
+        if (canvas.width !== width || canvas.height !== height) {
+            canvas.width = width;
+            canvas.height = height;
+            return true;
+        }
+        return false;
+    }
+    
+    function log(msg){
+        logLine++;
+        $("#log").prepend(logLine + ": " + msg + "\n");
+    }
+}
+
+
